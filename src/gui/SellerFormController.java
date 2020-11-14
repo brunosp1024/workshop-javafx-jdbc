812f446345bc8cad46231c26563900f5696d3d2d
brunosp1024 <brunosalvador145@gmail.com>
2020-11-14 17:51:15 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,137 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable{
+	
+	private Seller entity;
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	@FXML
+	private TextField txtName;
+	@FXML
+	private Button btnSave;
+	@FXML
+	private Button btnCancel;
+	@FXML
+	private Label labelErrorName;
+	
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	public void onBtnSaveAction(ActionEvent event) {
+		if (entity == null) { //Caso o programador esqueça de injetar o serviço, estoura uma exceção
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			utils.currentStage(event).close();
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	//Responsável por pegar os dados na caixa de formulário e instanciar um departamento.
+	//Retorna um departamento que esta associado com os campos do formulário.
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");
+
+		//o Id do form precisa ser convertido pra inteiro, pois o textField so recebe string.
+		obj.setId(utils.tryParseToInt(txtId.getText())); //Id do Departamento recebe o Id do formulário.
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty!");
+		}
+		
+		obj.setName(txtName.getText()); //Name do Departamento recebe o Name do formulário.
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+
+		return obj;
+	}
+
+	public void onBtnCancelAction(ActionEvent event) {
+		utils.currentStage(event).close();
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+		
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+
+}
