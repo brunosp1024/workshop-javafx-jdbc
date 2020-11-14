a9460142684307d4342dda31cb4b1cb867fc45d2
brunosp1024 <brunosalvador145@gmail.com>
2020-11-14 16:07:33 -0300

    SellerList

---
+++ src/gui/SellerListController.java
@@ -0,0 +1,173 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.IllegalSelectorException;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import model.entities.Seller;
+import model.services.SellerService;
+
+public class SellerListController implements Initializable, DataChangeListener {
+	
+	private SellerService service; 
+	
+	@FXML
+	private TableView<Seller> tableViewSeller;
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnId;
+	@FXML
+	private TableColumn<Seller, Integer> tableColumnName;
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnEDIT;
+	@FXML
+	private TableColumn<Seller, Seller> tableColumnREMOVE;
+	@FXML
+	private Button btnNew;
+	
+	private ObservableList<Seller> obsList;
+	
+	@FXML
+	public void onBtnNewAction(ActionEvent event) {
+		Stage parentStage = utils.currentStage(event);
+		Seller obj = new Seller();
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+	}
+	
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		//iniciando o comportamento das celulas na tabela
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		Stage stage = (Stage) Main.getMainScene().getWindow(); //pega a referencia da janela da aplicação.
+		tableViewSeller.prefHeightProperty().bind(stage.heightProperty()); //Faz a tabela acompnhar as dimensões da janela.
+	}
+	
+	public void updateTableView() {
+		if(service == null) { //Caso o programador esqueça de passar o serviço, extoura uma exceção;
+			throw new IllegalSelectorException();
+		}
+		List<Seller> list = service.findAll(); //Carrega o dados Mockados na classe SellerService.
+		obsList = FXCollections.observableArrayList(list); // Joga a lista para o objeto ObservableList.
+		tableViewSeller.setItems(obsList); //Atualiza a tableView com os dados.
+		initEditButtons();
+		initRemoveButtons();
+	}
+	
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+//		try {
+//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); //pega o arquivo fxml especificado pelo caminho.
+//			Pane pane = loader.load();
+//			
+//			SellerFormController controller = loader.getController();
+//			controller.setSeller(obj);
+//			controller.setSellerService(new SellerService());
+//			controller.subscribeDataChangeListener(this); //A  própria classe se inscreve para "escutar" o evento quando houver alteração nos dados.
+//			controller.updateFormData();
+//			
+//			Stage dialogStage = new Stage();
+//			dialogStage.setTitle("Enter Seller data");
+//			dialogStage.setScene(new Scene(pane));
+//			dialogStage.setResizable(false); //Não pode ser redimendionado.
+//			dialogStage.initOwner(parentStage); //adicionar o palco pai que chama o dialog.
+//			dialogStage.initModality(Modality.WINDOW_MODAL); //Travar e não permitir que outra tela seja acessado
+//			dialogStage.showAndWait();
+//		} catch (IOException e) {
+//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+//		}
+	}
+	
+	@Override
+	public void onDataChanged() { //Atualiza os dados na tabela do form quando o evento é disparado.
+		updateTableView();
+	}
+	
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/SellerForm.fxml", utils.currentStage(event)));
+			}
+		});
+	}
+	
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("remove");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Seller obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
+
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Service was null");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
+	}
+
+}
---
+++ src/model/services/SellerService.java
@@ -0,0 +1,32 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+	
+	private SellerDao dao = DaoFactory.createSellerDao();
+	
+	//Busca todos os departamentos no banco de dados
+	public List<Seller> findAll(){
+		return dao.findAll();
+	}
+	
+	//Salva ou atualiza no banco de dados
+	public void saveOrUpdate(Seller obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Seller obj) {
+		dao.deleteById(obj.getId());
+	}
+
+}
